282cc897b81208be93c289900147d904256a90a4
Alex Forest <azforest44@gmail.com>
2022-01-01 23:42:16 -0800

    strStr completed

--- leet_code/strings/strStr.js
+++ leet_code/strings/strStr.js
@@ -5,4 +5,24 @@
  */
  var strStr = function(haystack, needle) {
     if (needle.length === 0) return 0;
+    if (needle.length > haystack.length) return -1;
+    
+    for (let i = 0; i < haystack.length; i++) {
+        if (haystack[i] === needle[0]) {
+            if (needle.length === 1) return i;
+            else {
+                let sub = haystack.substring(i, i + needle.length);
+                if (sub.length < needle.length) continue;
+                if (needleCheck(sub, needle)) return i;
+            }
+        }
+    }
+    return -1;
 };
+
+var needleCheck = function(string, needle) {
+    for (let i = 0; i < string.length; i++) {
+        if (string[i] !== needle[i]) return false;
+    }
+    return true;
+}
