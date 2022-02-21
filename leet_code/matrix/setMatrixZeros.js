801e2a854dcaa12751005444420b9cf69418326e
Alex Forest <azforest44@gmail.com>
2022-02-20 22:40:07 -0800

    setMatrixZeros_correct

--- leet_code/matrix/setMatrixZeros.js
+++ leet_code/matrix/setMatrixZeros.js
@@ -6,16 +6,25 @@
     for (let y = 0; y < matrix.length; y++) { 
         for (let x = 0; x < matrix[0].length; x++) {
             if (matrix[y][x] === 0) {
-                modify(y, x);
-                matrix[y] = [9, 9, 9];
+                modifyUp(y, x);
+                modifyDown(y, x);
+                for (let z = 0; z < matrix[y].length; z++) {
+                    if (matrix[y][z] !== 0) matrix[y][z] = '0';
+                }
             }
         }
     }
 
-    function modify(y, x) {
-        matrix[y][x] = 9;
-        if (y + 1 < matrix.length) modify(y + 1, x);
-        if (y - 1 >= 0) modify(y - 1, x);
+    
+    function modifyUp(y, x) {
+        if (matrix[y][x] !== 0) matrix[y][x] = '0';
+        if (y + 1 < matrix.length) modifyUp(y + 1, x);
+    }
+    function modifyDown(y, x) {
+        if (matrix[y][x] !== 0) matrix[y][x] = '0';
+        if (y - 1 >= 0) modifyDown(y - 1, x);
     }
 };
+
+
 
